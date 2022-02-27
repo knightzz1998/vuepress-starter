c4ec62fefd41ef2fb466f5beaa5df85a8f743055
王天赐 <15565946702@163.com>
2022-02-27 11:23:02 +0800

    初始化vuepress项目

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    // 站点配置
+    lang: 'zh-CN',
+    title: '你好， VuePress ！',
+    description: '这是我的第一个 VuePress 站点',
+    // 主题和它的配置
+    theme: '@vuepress/theme-default',
+    themeConfig: {
+        logo: 'https://vuejs.org/images/logo.png',
+        nav: [
+            {
+                text: 'Group',
+                children: ['/group/README.md', '/group/test.md']
+            }
+        ],
+        sidebar: [
+            {
+                text: "Guide",
+                link: '/guide/',
+                children: ['/guide/README.md', '/guide/test.md']
+            }
+        ]
+
+    }
+
+}
---
+++ docs/guide/README.md
@@ -0,0 +1,3 @@
+# Hello GUIDE
+
+- 参考 [Vue Press添加页面](https://www.cnblogs.com/slightFly/p/12635584.html)
